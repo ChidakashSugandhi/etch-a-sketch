16325c75a5f87f43d1a102f9cc25cca6496d4a97
Chidakash S <99040053+ChidakashSugandhi@users.noreply.github.com>
2024-03-15 20:15:17 +0530

    change setAttribute style.backgroundColor

--- scripts.js
+++ scripts.js
@@ -5,6 +5,9 @@ const button = document.querySelector("button");
 
 rgbArray = ["red", "green", "blue"];
 
+
+// Both of the bellow methods work, in the first one we are completely resetting the style attribute in second one we are just changing backgroundColor style.
+
 button.addEventListener("click", () => {
      container.innerHTML = "";
      gridSize = prompt("Please Enter the Number of Squares per side.");
@@ -20,13 +23,35 @@ button.addEventListener("click", () => {
           pixelDiv.addEventListener("mouseover", (event) => {
                     // hovering changes the color to blue.
                     randomNumber = Math.floor(Math.random()*3);
-                    pixelDiv.setAttribute(`style`, `height: ${960/gridSize}px;
-                                                       width: ${960/gridSize}px;   
-                                                       background-color: ${rgbArray[randomNumber]};
-                                                       border: none;
-                                                       /* border: solid 1px black;  */
-                                                       flex: 0 0 auto;`);
+                    pixelDiv.style.backgroundColor = rgbArray[randomNumber]
                });
           container.appendChild(pixelDiv);
      }
 });
+
+
+// button.addEventListener("click", () => {
+//      container.innerHTML = "";
+//      gridSize = prompt("Please Enter the Number of Squares per side.");
+//      for (i = 0; i<gridSize*gridSize; i++){
+//           // Creating and setting the default grid properties.
+//           const pixelDiv = document.createElement("div");
+//           pixelDiv.setAttribute(`style`, `height: ${960/gridSize}px;
+//                                              width: ${960/gridSize}px;   
+//                                              background-color: black;
+//                                              border: none;
+//                                              /* border: solid 1px black;  */
+//                                              flex: 0 0 auto;`);
+//           pixelDiv.addEventListener("mouseover", (event) => {
+//                     // hovering changes the color to blue.
+//                     randomNumber = Math.floor(Math.random()*3);
+//                     pixelDiv.setAttribute(`style`, `height: ${960/gridSize}px;
+//                                                        width: ${960/gridSize}px;   
+//                                                        background-color: ${rgbArray[randomNumber]};
+//                                                        border: none;
+//                                                        /* border: solid 1px black;  */
+//                                                        flex: 0 0 auto;`);
+//                });
+//           container.appendChild(pixelDiv);
+//      }
+// });
